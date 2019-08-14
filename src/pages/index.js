470fdd8b7e97b1f6be7787c7bad2416324520611
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-08-14 17:40:16 -0500

    Adds some styling, a background image and main content background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,79 +5,81 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <section className="content">
-    <SEO
-      title="Chance Halo"
-      description="Personal site for Chance Embrey-Farquhar"
-    />
+  <div className="background">
+    <section className="content">
+      <SEO
+        title="Chance Halo"
+        description="Personal site for Chance Embrey-Farquhar"
+      />
 
-    {/* Header */}
-    <header>
-      <h1>Chance Embrey-Farquhar</h1>
-      <ul id="contact-links">
-        <li>
-          <a href="tel:+18163856369">
-            <FontAwesomeIcon icon={faMobileAlt} />
-          </a>
-        </li>
-        <li>
-          <a href="mailto:cembreyfarquhar@gmail.com">
-            <FontAwesomeIcon icon={faEnvelope} />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.github.com/cembreyfarquhar" target="_blank">
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/chance-embrey-farquhar/"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-        </li>
-      </ul>
-    </header>
-
-    {/* Main */}
-    <section className="main">
-      <section className="topic" id="objective">
-        <h2>What I'm up to</h2>
-        <div className="update-container">
-          <div className="update-box"> Some stuff</div>
-          <div className="update-box"> Other stuff</div>
-        </div>
-      </section>
-      <section className="topic" id="skills">
-        <h2>Technical Skills:</h2>
-        <ul id="skills-list">
-          <li>Javascript</li>
-          <li>Java</li>
-          <li>Python</li>
-          <li>React</li>
+      {/* Header */}
+      <header>
+        <h1>Chance Embrey-Farquhar</h1>
+        <ul className="contact-links">
+          <li>
+            <a href="tel:+18163856369">
+              <FontAwesomeIcon icon={faMobileAlt} />
+            </a>
+          </li>
+          <li>
+            <a href="mailto:cembreyfarquhar@gmail.com">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.github.com/cembreyfarquhar" target="_blank">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/chance-embrey-farquhar/"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </li>
         </ul>
-        <ul id="skills-list">
-          <li>Typescript</li>
-          <li>PostgreSQL</li>
-          <li>NodeJS</li>
-          <li>CSS3</li>
-        </ul>
-      </section>
+      </header>
 
-      <section className="topic" id="experience">
-        <h2>Relevant experience: </h2>
-        <ul id="exp-list">
-          <li>Lambda School</li>
-        </ul>
-      </section>
+      {/* Main */}
+      <section className="main">
+        <section className="topic" className="objective">
+          <h2>What I'm up to</h2>
+          <div className="update-container">
+            <div className="update-box"> Some stuff</div>
+            <div className="update-box"> Other stuff</div>
+          </div>
+        </section>
+        <section className="topic" className="skills">
+          <h2>Technical Skills:</h2>
+          <ul className="skills-list">
+            <li>Javascript</li>
+            <li>Java</li>
+            <li>Python</li>
+            <li>React</li>
+          </ul>
+          <ul className="skills-list">
+            <li>Typescript</li>
+            <li>PostgreSQL</li>
+            <li>NodeJS</li>
+            <li>CSS3</li>
+          </ul>
+        </section>
 
-      <footer>
-        <span>@Copyright 2019 Chance Embrey-Farquhar</span>
-      </footer>
+        <section className="topic" className="experience">
+          <h2>Relevant experience: </h2>
+          <ul className="exp-list">
+            <li>Lambda School</li>
+          </ul>
+        </section>
+
+        <footer>
+          <span>@Copyright 2019 Chance Embrey-Farquhar</span>
+        </footer>
+      </section>
     </section>
-  </section>
+  </div>
 )
 
 export default IndexPage
