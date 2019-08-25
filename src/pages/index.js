035705bf55aaeb334316e90b09e1916a457d6fad
Chance <cembreyfarquhar@gmail.com>
2019-08-25 12:29:31 -0500

    removed skills section

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,32 +33,8 @@ const IndexPage = () => {
       <Header />
       {/* Main */}
       <section className="main">
-        <section className="topic skills" id="skills">
-          <a href="#skills">
-            <h3>Technical Skills</h3>
-          </a>
-          {selectedSkill === null ? (
-            <ul className="skills-list">
-              <Skill skill={"Javascript"} selectSkill={selectSkill} />
-              <Skill skill={"Python"} selectSkill={selectSkill} />
-              <Skill skill={"React"} selectSkill={selectSkill} />
-              <Skill skill={"Typescript"} selectSkill={selectSkill} />
-              <Skill skill={"PostgreSQL"} selectSkill={selectSkill} />
-              <Skill skill={"NodeJS"} selectSkill={selectSkill} />
-              <Skill skill={"Java"} selectSkill={selectSkill} />
-              <Skill skill={"C#"} selectSkill={selectSkill} />
-            </ul>
-          ) : (
-            <Projects
-              skill={selectedSkill}
-              projects={projects}
-              selectSkill={selectSkill}
-            />
-          )}
-        </section>
-
         <section className="topic" id="experience">
-          <h3>More About Me</h3>
+          <h3>A brief introduction</h3>
           <p>
             I am a software engineer with a deep skillset in full-stack web
             development. I gradudated from Lambda School in August 2019. In my
@@ -68,6 +44,9 @@ const IndexPage = () => {
             complexity. I took two separate opportunites to become a Teaching
             Assistant at Lambda, during which I mentored multiple groups of new
             students on their path to becoming software developers.
+          </p>
+          <p>
+      
           </p>
         </section>
 
@@ -84,40 +63,4 @@ const IndexPage = () => {
   }
 }
 
-{
-  /* <header>
-<h1>Chance Embrey-Farquhar</h1>
-<h2>Full-Stack Web Developer</h2>
-<ul className="contact-links">
-  <li>
-    <a href="tel:+18163856369">
-      <FontAwesomeIcon icon={faMobileAlt} />
-    </a>
-  </li>
-  <li>
-    <a href="mailto:cembreyfarquhar@gmail.com">
-      <FontAwesomeIcon icon={faEnvelope} />
-    </a>
-  </li>
-  <li>
-    <a
-      href="https://www.github.com/cembreyfarquhar"
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <FontAwesomeIcon icon={faGithub} />
-    </a>
-  </li>
-  <li>
-    <a
-      href="https://www.linkedin.com/in/chance-embrey-farquhar/"
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <FontAwesomeIcon icon={faLinkedin} />
-    </a>
-  </li>
-</ul>
-</header> */
-}
 export default IndexPage
