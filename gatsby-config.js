7c07d9ef38d364f69d682998f231edd3996b32f2
Chance <cembreyfarquhar@gmail.com>
2019-08-25 12:37:42 -0500

    updates site description and other meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Web Developer`,
+    description: 'mv negativity.c positivity.py',
+    author: `@ChanceHalo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
