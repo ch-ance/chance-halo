97db84449a20b7c97896b3a93fe3319e94c8ab13
Chance <cembreyfarquhar@gmail.com>
2019-08-26 17:25:27 -0500

    projects page now links to github

---
+++ src/pages/projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <div>
+            <h1>Coming Soon. For now, visit my stared repositories on <a href="https://github.com/cembreyfarquhar" style={{color: "blue"}} target="_blank">GitHub</a></h1>
+        </div>
+    )
+}
+
+export default Projects;
