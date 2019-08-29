94972a566fc3afa391c52ecf5c142ab6fa8bbd36
halo <cembreyfarquhar@gmail.com>
2019-08-29 15:14:31 -0500

    fixes window undefined error by waiting until component mounts on TopLinkButton to grab window

--- src/components/TopLinkButton.js
+++ src/components/TopLinkButton.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React, { useEffect, useState } from "react"
+import { Link } from "gatsby"
 
 const TopLinkButton = () => {
+  const [toText, setToText] = useState(null)
+  const [toPath, setToPath] = useState(null)
 
-    const toText = window.location.pathname === "/contact" ? "Projects" : "Contact";
-    const toPath = window.location.pathname === "/contact" ? "projects" : "contact";
+  useEffect(() => {
+    setToText(window.location.pathname === "/contact" ? "Projects" : "Contact")
+    setToPath(window.location.pathname === "/contact" ? "projects" : "contact")
+  }, [])
 
-    console.log("TO PATH: ", toPath);
+  console.log("TO PATH: ", toPath)
 
-    return <Link to={toPath} className="top-link-button">Go to {toText}</Link>
+  return (
+    <Link to={toPath} className="top-link-button">
+      Go to {toText}
+    </Link>
+  )
 }
 
-export default TopLinkButton;
+export default TopLinkButton
