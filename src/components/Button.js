e18dc70a3394ad1404d19129fdc2dca8e81c2b45
halo <cembreyfarquhar@gmail.com>
2019-09-09 18:32:41 -0500

    nav button changes. not so shiny now

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-const style = {
-    width: "30%",
-    minHeight: '50px',
-    borderRadius: '5px',
-    backgroundImage: "linear-gradient(to bottom right, #FA5A55, #FA55A3)",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    fontWeight: "bold",
-    fontSize: ".8rem"
-}
-
 const Button = (
     { to }
 ) => (
     // <Link to={to} style={style}>{to.toUpperCase()}</Link>
-    <Link to={to} style={style}>Let's Go >></Link>
+    <Link to={to} className="landing-button">Let's Go >></Link>
     )
 
 export default Button;
