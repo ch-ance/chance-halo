110e4b587ee60a1819b91552dab10ae76bfdc2ef
halo <cembreyfarquhar@gmail.com>
2019-09-09 17:04:17 -0500

    navigation looks more proper - removed ugly buttons

--- src/components/TopLinkButton.js
+++ src/components/TopLinkButton.js
@@ -1,23 +1,19 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { Link } from "gatsby"
 
 const TopLinkButton = () => {
-  const [toText, setToText] = useState(null)
-  const [toPath, setToPath] = useState(null)
-  const [loaded, setLoaded] = useState(false)
-
-  useEffect(() => {
-    setToText(window.location.pathname === "/contact" ? "Projects" : "Contact")
-    setToPath(window.location.pathname === "/contact" ? "projects" : "contact")
-    setLoaded(true);
-  }, [])
-
   return (
-    loaded ? 
-    <Link to={toPath} className="top-link-button">
-      Go to {toText}
-    </Link>
-    : <h4>Loading</h4>
+    <div className="top-link-buttons">
+      <Link to={"/projects"} className="top-link-button">
+        View Projects
+      </Link>
+      <Link to={"/"} className="top-link-button">
+        Home
+      </Link>
+      <Link to={"/contact"} className="top-link-button">
+        Contact Me
+      </Link>
+    </div>
   )
 }
 
