0d069a2c5b091d7c5e4671925de1ddc8e10fbe3b
halo <cembreyfarquhar@gmail.com>
2019-09-10 15:15:44 -0500

    mend

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Web Developer`,
     description: 'rm -rf bad_things.* && touch good_things.js',
     author: `Chance Embrey-Farquhar`,
-    image: 'src/images/gatsby-icon.png',
+    image: '/src/images/gatsby-icon.png',
     url: 'https://chancehalo.me',
     twitterUsername: '@ChanceHalo'
   },
@@ -28,7 +28,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `/src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
